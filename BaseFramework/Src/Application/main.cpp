8d6452bd6085b2b9f45043ef72c9c9819c25e1b8
ParuParuParu <kd1363179@st.kobedenshi.ac.jp>
2024-04-17 10:54:04 +0900

    消したやつ
    消した

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,10 +221,6 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
-	//後で絶対消す
-
-	m_test = 10;
-
 	return true;
 
 }
